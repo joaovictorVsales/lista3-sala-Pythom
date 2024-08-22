f9e478ff5ba74d73e4b1f89f448ddd968a150f20
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2024-08-22 00:17:42 -0300

    Add files via upload

---
+++ EX10.py
@@ -0,0 +1,6 @@
+N= 0
+for P in range(501):
+    if P %2 == 0:
+       N=P+N
+       print(N)
+       
---
+++ EX9.py
@@ -0,0 +1,4 @@
+print('POTENCIAÇÃO')
+N= int(input('Digite um número: '))
+P= int(input('Digite a potencia do número: '))
+print(f'O resultado é {N**P}')
